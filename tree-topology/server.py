e8f54561a71652d14837a38b0e8d752e60900094
rmahendra9 <ritvik.mahendra@utexas.edu>
2023-10-09 04:58:48 -0500

    added rudimentary tree topology implementation

---
+++ tree-topology/server.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import signal
+import sys
+
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print(f'Received data from client: {data.decode("utf-8")}')
+    client_socket.close()
+
+def shutdown_server(sig, frame):
+    print("Shutting down server...")
+    server.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, shutdown_server)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 8081))
+server.listen(2)
+print("Listening on 8081")
+
+while True:
+    client, addr = server.accept()
+    print('Accepted cxn from {addr[0]}:{addr[1]}')
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
