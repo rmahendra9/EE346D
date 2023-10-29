4794c86351b59e427b4b35d3393741e7267117cd
SanjayGorurUT <SanjayGorur2001@utexas.edu>
2023-10-28 21:14:11 -0500

    created simple ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='title'>
+        <h1>Data Aggregation Networks for Federated Learning</h1>
+      </div>
+      <div className="dashboard-box">
+        <h2>Model Outputs</h2>
+        <div className="dashboard-item">
+          <h4>Model Visualization</h4>
+          {/* Add your model visualization component here */}
+        </div>
+
+        <div className="dashboard-item">
+          <h4>Metrics</h4>
+          {/* Add your log outputs component here */}
+        </div>
+        
+        <div className="dashboard-item">
+          <h4>Data Preview</h4>
+          {/* Add your data preview component here */}
+        </div>
+
+        <div className="dashboard-item">
+          <h4>Log Outputs</h4>
+          {/* Add your log outputs component here */}
+        </div>
+      </div>
     </div>
   );
 }
